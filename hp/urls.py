867d8fed4840ff1c1f2f2e312a98e11ba4d04319
Neeky <1721900707@qq.com>
2020-03-31 17:36:09 +0800

    first commit

---
+++ hp/urls.py
@@ -0,0 +1,32 @@
+"""hp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.views.generic.base import View
+from django.http.response import JsonResponse
+
+
+class HelloView(View):
+    def get(self, *args, **kwargs):
+        """
+        """
+        return JsonResponse({'message': 'hello-world'})
+
+
+urlpatterns = [
+    path('hello', HelloView.as_view(), name="hello-world"),
+]
